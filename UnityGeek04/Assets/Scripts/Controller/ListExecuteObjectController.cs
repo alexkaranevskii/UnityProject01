c8fcc6d57801587bf7cfa2bff9756ea69fa21b78
Alexey Karanevski <alexkaranevskii@mail.ru>
2022-12-05 12:23:48 +0300

    Версия перед добавлением перечислений

--- UnityGeek04/Assets/Scripts/Controller/ListExecuteObjectController.cs
+++ UnityGeek04/Assets/Scripts/Controller/ListExecuteObjectController.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using System;
 using UnityEngine;
 
@@ -6,12 +7,15 @@ namespace Geek
 {
     public class ListExecuteObjectController
     {
-        private IExecute[] _interactiveObject;
+        private IExecute[] _interactiveObject; // массив содержит экземпляры классов, которые реализуют интерфейс IExecute
 
         public int Length { get { return _interactiveObject.Length; } }   
+
         
         public IExecute[] InteractiveObject { get => _interactiveObject; set => _interactiveObject = value; }
+
     
+        // Конструктор
         public ListExecuteObjectController()
         {
 
@@ -19,14 +23,14 @@ namespace Geek
 
         public void AddExecuteObject(IExecute execute)
         {
-            if(_interactiveObject == null)
+            if(InteractiveObject == null)
             {
-                _interactiveObject = new[] {execute };
+                InteractiveObject = new[] {execute };
                 return;
             }
 
             Array.Resize(ref _interactiveObject, Length + 1);
-            _interactiveObject[Length - 1] = execute;
+            InteractiveObject[Length - 1] = execute;
         }
     }
 }
